a434743d3104bb4b039ec08db35306e609df51b0
Duviviers <emmanuelduviviers49@hotmail.com>
2013-04-07 22:10:50 +0200

    Update settings.py

--- server/phpcafe/settings.py
+++ server/phpcafe/settings.py
@@ -14,7 +14,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'phpcafe',                      # Or path to database file if using sqlite3.
         'USER': 'phpcafe',                      # Not used with sqlite3.
-        'PASSWORD': 'php_Caf3',                  # Not used with sqlite3.
+        'PASSWORD': '*******',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
